5f9ce7bf7ab29fa217d1457dbe7713131bb28971
Drew Josey <johnajosey@gmail.com>
2018-06-27 10:12:54 -0500

    Updated fucntionality, but the access token seems to now be broken.  Need to set log in/access token credentials to update continuously.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 import './App.css';
+import Profile from './Profile.jsx'
+import Gallery from './Gallery.jsx'
 
 class App extends Component{
 
@@ -9,33 +11,38 @@ class App extends Component{
     super(props);
     this.state = {
       query: '',
-      artist: null
+      artist: null,
+      tracks: []
     }
   }
 
   search() {
           console.log('this.state', this.state);
           const BASE_URL = 'https://api.spotify.com/v1/search?';
-          const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+          let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+          const ALBUM_URL = 'https://api.spotify.com/v1/artists/'
           let accessToken = 'BQALeDiMAHQ6isyHoRQkf9zMwWHmWI03CB--kVZ0nSGXbBymeJN17M4zxt2gOZg0NJ28Xz3b4p1CpwEGTR_J9KVWtCd3rfQincQytDo5-7OMgVKSLyC3AJKMMq8QdBwCPPkIuZQTfUXu7T-1mPc8t0ZpcAaTKUJjvg&refresh_token=AQAxuir5iJ0qdZz9XStP2ql-0bFws9hDTJS2zSsDg8JKaZGDIo6O8Bdjr1Ir72UbUJTC4hgEU2NQQth4zThPHv3aysxWniNoHJ8SWFYLtTz5mJ9PIg1CRVEFrr2ydI-zdZI'
           console.log('Fetch_URL', FETCH_URL);
-
-          var myOptions = {
-                 method: 'GET',
-                 headers: {
-                     'Authorization': 'Bearer ' + accessToken
-                 },
-                 mode: 'cors',
-                 cache: 'default'
-             };
 
           fetch(FETCH_URL, {
             method: 'GET'
           })
           .then(response => response.json())
-          .then(json => {
+          .then(json =>{
             const artist = json.artists.items[0];
-            console.log('artist', artist);
+            this.setState({artist});
+
+            FETCH_URL = `${ALBUM_URL}${artist.id}/top-tracks?country=US&`
+
+            fetch(FETCH_URL, {
+              method: 'GET'
+            })
+            .then(response => response.json())
+            .then(json => {
+              console.log('artist\'s top tracks:', json);
+              const {tracks} = json;
+              this.setState({tracks});
+            })
           });
       }
 
@@ -61,15 +68,21 @@ class App extends Component{
             </InputGroup.Addon>
           </InputGroup>
         </FormGroup>
-        <div className="Profile">
-          <div>Artist Picture</div>
-          <div>Artist Name</div>
+        {
+        this.state.artist !== null
+        ?
+        <div>
+         <Profile
+          artist={this.state.artist}
+         />
+         <Gallery
+           tracks={this.state.tracks}
+         />
         </div>
-        <div className="Gallery">
-          Gallery
-        </div>
-      </div>
+        : <div></div>
+      }
 
+    </div>
     )
   }
 }
